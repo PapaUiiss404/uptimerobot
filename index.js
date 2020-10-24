7fa7010929155932426337c342e42a525d7c90a9
PapaUiiss <53428072+PapaUiiss404@users.noreply.github.com>
2020-10-24 13:20:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["link project"]
+const urls = ["https://cloudy-invented-book.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
